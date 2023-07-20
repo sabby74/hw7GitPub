65657d383e80f03d1dc387fd07f89f6e1197297e
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-20 14:01:50 -0400

    setting up database

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/" , (req,res) =>{
-    res.send("Hello i am here");
+    res.send("Welcome to Gitpub App!");
 });
 
 app.listen(3000 ,() => {
