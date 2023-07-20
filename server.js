58bc239d1c344fc5ff20594fa37cdc27d2b54129
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-20 13:56:33 -0400

    server set up

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express");
+
+const app = express();
+
+app.get("/" , (req,res) =>{
+    res.send("Hello i am here");
+});
+
+app.listen(3000 ,() => {
+    console.log("server is listening on port 3000");
+})
