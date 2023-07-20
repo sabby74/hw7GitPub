bb4ca8603069aa52c603a6f1bdaf5789b5c96327
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-07-20 14:54:34 -0400

    get route for drinks

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 
 const app = express();
+const drinks = require("./models/drinks.js")
+
 
 app.get("/" , (req,res) =>{
     res.send("Welcome to Gitpub App!");
 });
 
+app.get("/drinks", (req,res) =>{
+    res.send(drinks)
+})
+
+
 app.listen(3000 ,() => {
     console.log("server is listening on port 3000");
-})
+});
